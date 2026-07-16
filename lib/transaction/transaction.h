d5c59bebc099fb8addf5fdf4092ed76a869b30ea
Jerome-abba Emmanuel <106757046+jerome-ae@users.noreply.github.com>
2026-07-16 13:53:33 +0100

    Add TX_NOT_TIME_SYNCED status and time sync check

--- lib/transaction/transaction.h
+++ lib/transaction/transaction.h
@@ -4,11 +4,13 @@
 #include "../storage/storage.h"
 
 typedef enum { 
-    TX_RECORDED = 0, 
-    TX_LOG_FULL = 1, 
-    TX_ERROR = -1 
+    TX_RECORDED         = 0, 
+    TX_LOG_FULL         = 1, 
+    TX_NOT_TIME_SYNCED  = 2,
+    TX_ERROR            = -1 
 } TransactionResult;
 
 void              transaction_set_rtc(unsigned long unix_ts);
 unsigned long     transaction_get_ts();
+bool              transaction_time_synced();
 TransactionResult transaction_record(const char* uid, const char* drv_uid);
